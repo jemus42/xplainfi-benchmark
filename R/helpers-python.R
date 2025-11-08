65324f36fb64f0975576949f007b972d821915ff
Lukas Burk <lukas@quantenbrot.de>
2025-11-08 16:41:44 +0100

    xgb rounds default

--- R/helpers-python.R
+++ R/helpers-python.R
@@ -148,6 +148,7 @@ create_sklearn_learner <- function(
 		# Note: early_stopping_rounds requires validation data in fit()
 		if (task_type == "regr") {
 			learner <- xgb$XGBRegressor(
+				n_estimators = 1000L,
 				learning_rate = 0.1,
 				booster = "gbtree",
 				tree_method = "hist",
@@ -157,6 +158,7 @@ create_sklearn_learner <- function(
 			)
 		} else {
 			learner <- xgb$XGBClassifier(
+				n_estimators = 1000L,
 				learning_rate = 0.1,
 				booster = "gbtree",
 				tree_method = "hist",
