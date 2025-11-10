e57d21c2534b7976dd76ec477333deffa282d606
Lukas Burk <burk@leibniz-bips.de>
2025-11-10 19:15:12 +0100

    No early stopping in runtime benchmark

--- config-runtime.R
+++ config-runtime.R
@@ -18,7 +18,7 @@ conf <- list(
 	n_repeats = c(1, 50, 100),
 	# For SAGE permutations
 	n_permutations = c(5, 10, 50),
-	sage_early_stopping = c(TRUE, FALSE),
+	sage_early_stopping = FALSE,
 	# Size of sampled data used for Monte Carlo integration in SAGE methods
 	sage_n_samples = c(10, 200, 500),
 	# Types of learners to use for each method, uses create_learner helper
