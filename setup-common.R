09538d6144f5e3b76567e72c8cce518b4d272179
Lukas Burk <burk@leibniz-bips.de>
2025-11-12 17:24:23 +0100

    Tweak missing package detection

--- setup-common.R
+++ setup-common.R
@@ -26,14 +26,11 @@ local({
 		"fs"
 	)
 
-	missing_pks <- all(sapply(
-		packages,
-		requireNamespace,
-		quietly = TRUE
-	))
-	if (!missing_pks) {
+	missing_pks <- setdiff(packages, rownames(installed.packages()))
+
+	if (length(missing_pks) > 0) {
 		cli::cli_warn(c(
-			"!" = "Not all required packages are installed",
+			"!" = "Not all required packages are installed, missing {.val {missing_pks}}",
 			i = "Run {.code rv sync} in the terminal to sync dependencies.",
 			"See README.md"
 		))
